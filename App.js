e3ede34d52f1bbe65892c83b3b43e4e117637153
Renato Silva <dev.renatosilva@gmail.com>
2022-11-20 10:44:51 -0300

    'feat:add-fonts-and-splash'

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [appLoaded, setAppLoaded] = useState(false);
+
+  useEffect(() => {
+
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          "black": require('./assets/fonts/Roboto-Black.ttf'),
+          "regular": require('./assets/fonts/Roboto-Regular.ttf'),
+          "italic": require('./assets/fonts/Roboto-Italic.ttf')
+        });
+      } catch (error) {
+        console.log.error(error);
+      } finally {
+        setAppLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appLoaded])
+
+  if(!appLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider 
+      style={styles.container} 
+      onLayout={onLayout}>
       <SafeAreaView>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.label}>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </SafeAreaView>
     </SafeAreaProvider>
@@ -19,4 +60,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: 'italic'
+  }
 });
